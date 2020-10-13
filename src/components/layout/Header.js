8c781f25be1aff328bd39b31d8d39490ae35a13b
cecilia1982 <ercolemc1982@gmail.com>
2020-10-12 21:06:57 -0300

    Primeras configuraciones de la APP

---
+++ src/components/layout/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FaPizzaSlice } from 'react-icons/fa';
+
+
+export const Header = () => {
+const x = 1;
+
+return (
+
+<header className="header" data-testid="header">
+<nav>
+	<div className="logo">
+		<img src="/images/logo.png" alt="Todomoylist"/>
+	</div>
+	<div className="settings">
+		<ul>
+			<li>+</li>
+			<li>
+				<FaPizzaSlice />
+
+			</li>
+		</ul>
+
+	</div>
+</nav>
+
+</header>
+
+
+
+);
+
+};
---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Sidebar = () => <p>Soy un Sidebar</p>;
